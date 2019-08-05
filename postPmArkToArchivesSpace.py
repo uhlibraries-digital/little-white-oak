dab684f134e60185509b4370f7b7a2eb9765a305
Sean Watkins <slwatkins@uh.edu>
2019-08-05 10:42:31 -0500

    Update File Versions for preservation ark
    Preservation objects will no longer have its own digital object. Instead a single digital object will be used with a preservation/access file version.

--- postPmArkToArchivesSpace.py
+++ postPmArkToArchivesSpace.py
@@ -33,6 +33,7 @@ def get_pm_ark_and_aspace_uri(job, baseDirectory):
     ark = ''
     uri = ''
     title = ''
+    do_uuid = ''
     for entry, data in metadata.items():
         for name, values in data.items():
             if name == 'dcterms.identifier' and ark == '':
@@ -41,8 +42,10 @@ def get_pm_ark_and_aspace_uri(job, baseDirectory):
                 uri = values[0]
             if name == 'dcterms.title' and title == '':
                 title = values[0]
+            if name == 'uhlib.doUuid' and do_uuid == '':
+                do_uuid = values[0]
 
-    return {'ark': ark, 'uri': uri, 'title': title}
+    return {'ark': ark, 'uri': uri, 'do_uuid': do_uuid, 'title': title}
 
 def get_pm_ark_in_values(values):
     for value in values:
@@ -54,81 +57,97 @@ def get_pm_ark_in_values(values):
 def post_pm_ark(job, data):
     global minter_base
 
-    new_digital_object = {
-        "jsonmodel_type": "digital_object",
-        "digital_object_id": str(uuid.uuid4()),
-        "external_ids": [],
-        "subjects": [],
-        "linked_events": [],
-        "extends": [],
-        "dates": [],
-        "external_documents": [],
-        "rights_statements": [],
-        "linked_agents": [],
-        "file_versions": [
-            {
-                "jsonmodel_type": "file_version",
-                "is_representative": False,
-                "file_uri": minter_base + data['ark'],
-                "use_statement": "",
-                "xlink_actuate_attribute": "",
-                "xlink_show_attribute": "",
-                "file_format_name": "",
-                "file_format_version": "",
-                "checksum": "",
-                "checksum_method": "",
-                "publish": False
-            }
-        ],
-        "restrictions": False,
-        "notes": [],
-        "linked_instances": [],
-        "title": data['title'] + " (Preservation)",
-        "language": "",
-        "publish": False
-    }
     archival_object = aspace_request(data['uri'])
 
+    new_digital_object = False
+    digital_object = get_digital_object(archival_object['instances'], data['do_uuid'])
+    if digital_object == None:
+        new_digital_object = True
+        digital_object = {
+            "jsonmodel_type": "digital_object",
+            "digital_object_id": data['do_uuid'],
+            "external_ids": [],
+            "subjects": [],
+            "linked_events": [],
+            "extends": [],
+            "dates": [],
+            "external_documents": [],
+            "rights_statements": [],
+            "linked_agents": [],
+            "file_versions": [],
+            "restrictions": False,
+            "notes": [],
+            "linked_instances": [],
+            "title": data['title'],
+            "language": "",
+            "publish": False
+        }
+
     # Need to check if the archival object already has a PM Ark
-    if has_pm_ark(archival_object['instances'], data['ark']):
+    if has_pm_ark(digital_object['file_versions'], data['ark']):
         job.pyprint("Archival Object already has a PM Ark of {}".format(data['ark']))
         return 0
+    
+    digital_object['file_versions'].append({
+        "jsonmodel_type": "file_version",
+        "is_representative": False,
+        "file_uri": minter_base + data['ark'],
+        "use_statement": "Preservation",
+        "xlink_actuate_attribute": "",
+        "xlink_show_attribute": "",
+        "file_format_name": "",
+        "file_format_version": "",
+        "checksum": "",
+        "checksum_method": "",
+        "publish": False
+    })
 
-    try:
-        response_do = aspace_request(archival_object['repository']['ref'] + '/digital_objects', json.dumps(new_digital_object))
-    except UnicodeDecodeError:
-        job.pyprint("Unable to post digital object. You have non-utf8 characters in you metadata.csv file: {}".format(new_digital_object),file=sys.stderr)
-        return 1
-    except:
-        job.pyprint ("Unable to post digital object",file=sys.stderr)
-        return 1
+    if new_digital_object:
+        try:
+            response_do = aspace_request(archival_object['repository']['ref'] + '/digital_objects', json.dumps(digital_object))
+        except UnicodeDecodeError:
+            job.pyprint("Unable to post digital object. You have non-utf8 characters in you metadata.csv file: {}".format(digital_object),file=sys.stderr)
+            return 1
+        except:
+            job.pyprint ("Unable to post digital object",file=sys.stderr)
+            return 1
 
-    new_instance = {
-        "jsonmodel_type": "instance",
-        "digital_object": {
-            "ref": response_do['uri']
-        },
-        "instance_type": "digital_object",
-        "is_representative": False
-    }
+        archival_object['instances'].append({
+            "jsonmodel_type": "instance",
+            "digital_object": {
+                "ref": response_do['uri']
+            },
+            "instance_type": "digital_object",
+            "is_representative": False
+        })
 
-    archival_object['instances'].append(new_instance)
-
-    response = aspace_request(data['uri'], json.dumps(archival_object))
+        response = aspace_request(data['uri'], json.dumps(archival_object))
+    else:
+        try:
+            response_do = aspace_request(digital_object['uri'], json.dumps(digital_object))
+        except:
+            job.pyprint("Unable to update digital object: {}".format(digital_object),file=sys.stderr)
+            return 1
 
     return 0
 
 
-def has_pm_ark(instances, ark):
+def has_pm_ark(file_versions, ark):
     ark = ark.replace('/', '\/')
+    for do_file in file_versions:
+        if re.match('^.*' + ark + '$', do_file['file_uri']):
+            return True
+
+    return False
+
+def get_digital_object(instances, uuid):
     for instance in instances:
         if instance['instance_type'] == 'digital_object':
             do = aspace_request(instance['digital_object']['ref'])
-            for do_file in do['file_versions']:
-                if re.match('^.*' + ark + '$', do_file['file_uri']):
-                    return True
-    return False
-
+            if do['digital_object_id'] == uuid:
+                return do
+    
+    return None
 
 def get_aspace_session(job):
     global aspace_endpoint, aspace_username, aspace_password
@@ -173,6 +192,9 @@ def post_aspace(job):
         return 0
     if data['uri'] == '':
         job.pyprint("No ArchivesSpace uri found. Move along, nothing to see here")
+        return 0
+    if data['do_uuid'] == '':
+        job.pyprint("No ArchivesSpace digital object UUID. Move along, nothing to see here")
         return 0
 
     job.pyprint("Found PM Ark: {}".format(data['ark']))
